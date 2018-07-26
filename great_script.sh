258d25bb0625afa2d76571352e65c7154dc58718
Matt Wiens <mwiens91@gmail.com>
2018-07-26 12:04:29 -0700

    Fix path in great_script.py

--- great_script.sh
+++ great_script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python query_gsc_for_wgs_bams.py '{"json_data": "results.json", "library_ids": ["A91716", "A91717"], "skip_file_import": true, "skip_older_than": "2015-07-24"}'
+python automate_me/query_gsc_for_wgs_bams.py '{"json_data": "results.json", "library_ids": ["A91716", "A91717"], "skip_file_import": true, "skip_older_than": "2015-07-24"}'
